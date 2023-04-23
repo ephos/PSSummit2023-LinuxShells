3902f7063f88711198d49a82327308784b091b21
Rob Pleau <rjpleau@gmail.com>
2023-04-23 10:18:37 -0400

    Changed PSReadLine fir to be ISE colors

--- demo/0-Setup/psreadline_fix.ps1
+++ demo/0-Setup/psreadline_fix.ps1
@@ -1,14 +1,35 @@
-Set-PSReadLineOption -Colors @{
-   "Default"   = [ConsoleColor]::Black
-   "Parameter" = [ConsoleColor]::Black
-   "Type"      = [ConsoleColor]::DarkBlue
-   "Number"    = [ConsoleColor]::Magenta
-   "String"    = [ConsoleColor]::Magenta
-   "Comment"   = [ConsoleColor]::Yellow
-   "Variable"  = [ConsoleColor]::Green
-   "Keyword"   = [ConsoleColor]::Blue
-   "Operator"  = [ConsoleColor]::Blue
-   "Command"   = [ConsoleColor]::Blue
-   "Member"    = [ConsoleColor]::Blue
-   "Error"     = [ConsoleColor]::Red
+$ISETheme = @{
+    Command                  = $PSStyle.Foreground.FromRGB(0x0000FF)
+    Comment                  = $PSStyle.Foreground.FromRGB(0x006400)
+    ContinuationPrompt       = $PSStyle.Foreground.FromRGB(0x0000FF)
+    Default                  = $PSStyle.Foreground.FromRGB(0x0000FF)
+    Emphasis                 = $PSStyle.Foreground.FromRGB(0x287BF0)
+    Error                    = $PSStyle.Foreground.FromRGB(0xE50000)
+    InlinePrediction         = $PSStyle.Foreground.FromRGB(0x93A1A1)
+    Keyword                  = $PSStyle.Foreground.FromRGB(0x00008b)
+    ListPrediction           = $PSStyle.Foreground.FromRGB(0x06DE00)
+    Member                   = $PSStyle.Foreground.FromRGB(0x000000)
+    Number                   = $PSStyle.Foreground.FromRGB(0x800080)
+    Operator                 = $PSStyle.Foreground.FromRGB(0x757575)
+    Parameter                = $PSStyle.Foreground.FromRGB(0x000080)
+    String                   = $PSStyle.Foreground.FromRGB(0x8b0000)
+    Type                     = $PSStyle.Foreground.FromRGB(0x008080)
+    Variable                 = $PSStyle.Foreground.FromRGB(0xff4500)
+    ListPredictionSelected   = $PSStyle.Background.FromRGB(0x93A1A1)
+    Selection                = $PSStyle.Background.FromRGB(0x00BFFF)
 }
+Set-PSReadLineOption -Colors $ISETheme
+#Set-PSReadLineOption -Colors @{
+#   "Default"   = [ConsoleColor]::Black
+#   "Parameter" = [ConsoleColor]::Black
+#   "Type"      = [ConsoleColor]::DarkBlue
+#   "Number"    = [ConsoleColor]::Magenta
+#   "String"    = [ConsoleColor]::Magenta
+#   "Comment"   = [ConsoleColor]::Yellow
+#   "Variable"  = [ConsoleColor]::Green
+#   "Keyword"   = [ConsoleColor]::Blue
+#   "Operator"  = [ConsoleColor]::Blue
+#   "Command"   = [ConsoleColor]::Blue
+#   "Member"    = [ConsoleColor]::Blue
+#   "Error"     = [ConsoleColor]::Red
+#}
